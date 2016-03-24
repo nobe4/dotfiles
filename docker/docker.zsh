a1df162c0febc398b1e5f9b093ce4c502416945d
nobe4 <haffreinguev@gmail.com>
2016-03-24 07:33:55 +0000

    Change docker.zsh source method

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -1,8 +1,10 @@
-# Check if docker exist before adding the env
-if hash docker 2>/dev/null; then
-  eval "$(docker-machine env default)"
+# Check if docker exist before adding the env and aliases
+if ! hash docker 2>/dev/null; then
+  alias dk="echo 'Please install docker first'"
+  return
 fi
 
+eval "$(docker-machine env default)"
 alias dk="docker"
 alias dc="docker-compose"
 alias dkip="docker-machine ip default | /usr/bin/tr -d '\n' | tee >(pbcopy) | xargs echo copied$1"
