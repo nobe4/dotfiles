b83ac0d9fc83a4c3c0428f1fed1b0c3a3a767734
nobe4 <haffreinguev@gmail.com>
2016-03-24 07:51:07 +0000

    Indent and add guard for vagrant aliases

--- vagrant/aliases.zsh
+++ vagrant/aliases.zsh
@@ -1,44 +1,46 @@
+if ! hash vagrant 2>/dev/null; then; return; fi
+
 # vagrant alias
 alias vre="vagrant halt && tm vagrant up"
 
 # Check if the current folder has a vagrant environment
 _vagrant_check(){
-	vagrantStatus="$(vagrant status 2>&1)"
-	if echo $vagrantStatus | grep -q 'environment' ; then
-		echo 'Not a vagrant environment, exiting ...'
-		return 1
+  vagrantStatus="$(vagrant status 2>&1)"
+  if echo $vagrantStatus | grep -q 'environment' ; then
+    echo 'Not a vagrant environment, exiting ...'
+    return 1
   else
     return 0
-	fi
+  fi
 }
 
 _vagrant_is_up(){
-	vagrantStatus="$(vagrant status 2>&1)"
-	if echo $vagrantStatus | grep -q 'saved\|aborted\|poweroff' ; then
-		return 1
+  vagrantStatus="$(vagrant status 2>&1)"
+  if echo $vagrantStatus | grep -q 'saved\|aborted\|poweroff' ; then
+    return 1
   else
     return 0
-	fi
+  fi
 }
 
 vup(){
-	if ! _vagrant_check; then return; fi
-	if ! _vagrant_is_up; then
-		vagrant up
-	fi
+  if ! _vagrant_check; then return; fi
+  if ! _vagrant_is_up; then
+    vagrant up
+  fi
   notify "Vagrant started"
 }
 
 vhalt(){
-	if ! _vagrant_check; then return; fi
-	if _vagrant_is_up; then
-		vagrant halt
-	fi
+  if ! _vagrant_check; then return; fi
+  if _vagrant_is_up; then
+    vagrant halt
+  fi
   notify "Vagrant halted"
 }
 
 vsh(){
   vup
-	vagrant ssh
+  vagrant ssh
 }
 
