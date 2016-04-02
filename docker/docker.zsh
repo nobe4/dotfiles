7d33ac51a0c49cf42bb2227632b49e83d7dbee94
nobe4 <haffreinguev@gmail.com>
2016-04-03 00:27:36 +0100

    Escape $ for alias

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -14,7 +14,7 @@ alias dc="docker-compose"
 
 alias dkip="docker-machine ip default | /usr/bin/tr -d '\n' | tee >(pbcopy) | xargs echo copied$1"
 
-alias dkrmxt="docker rm $(docker ps -q -f status=exited)"
+alias dkrmxt="docker rm \$(docker ps -q -f status=exited)"
 alias dkrmidg="docker images --quiet --filter=dangling=true | xargs docker rmi"
 
 alias dkenv="eval \"$(docker-machine env default)\""
