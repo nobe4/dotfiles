9a0ccd607b2549d47ec67f61995edcf168970a8e
nobe4 <haffreinguev@gmail.com>
2016-04-02 21:21:40 +0100

    Add a bunch of docker aliases

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -5,6 +5,18 @@ if ! hash docker 2>/dev/null; then
 fi
 
 eval "$(docker-machine env default)"
+
 alias dk="docker"
 alias dc="docker-compose"
+
 alias dkip="docker-machine ip default | /usr/bin/tr -d '\n' | tee >(pbcopy) | xargs echo copied$1"
+
+alias dkrmxt="docker rm $(docker ps -q -f status=exited)"
+alias dkrmidg="docker images --quiet --filter=dangling=true | xargs docker rmi"
+
+alias dkenv="eval \"$(docker-machine env default)\""
+
+dksh(){
+  docker exec -it $1 bash
+}
+
