a25edc6c8b4b9af484a617fbf30c867d5d43330f
nobe4 <haffreinguev@gmail.com>
2016-04-03 22:40:12 +0100

    better docker images deletion and add busybox

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -15,7 +15,7 @@ alias dc="docker-compose"
 alias dkip="docker-machine ip default | /usr/bin/tr -d '\n' | tee >(pbcopy) | xargs echo copied$1"
 
 alias dkrmxt="docker rm \$(docker ps -q -f status=exited)"
-alias dkrmidg="docker images --quiet --filter=dangling=true | xargs docker rmi"
+alias dkrmidg="docker images --all | awk '/^<none>/ {print $3}' | xargs docker rmi"
 
 alias dkenv="eval \"$(docker-machine env default)\""
 
@@ -23,3 +23,4 @@ dksh(){
   docker exec -it $1 bash
 }
 
+alias bbox="docker run -it --rm busybox"
