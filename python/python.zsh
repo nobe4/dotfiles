6ec46b535972a55de469bdd1ef63ca14053fe7cf
nobe4 <haffreinguev@gmail.com>
2016-04-15 11:22:31 +0100

    Use foreman for debug, so env keys will be loaded

--- python/python.zsh
+++ python/python.zsh
@@ -4,6 +4,6 @@ virtualenvwrapper='/usr/local/bin/virtualenvwrapper.sh'
 [ -f $virtualenvwrapper ] && source $virtualenvwrapper
 
 alias pyserve='foreman start'
-alias pyservedebug='python -m pdb manage.py runserver'
+alias pyservedebug='foreman run python -m pdb manage.py runserver'
 
 alias pyman='python manage.py'
