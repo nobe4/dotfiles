2484248fcde0d96583ed65426718e70414c995f0
nobe4 <haffreinguev@gmail.com>
2016-10-05 10:48:02 +0100

    Remove unused antigen plugins

--- antigen/antigen.zsh
+++ antigen/antigen.zsh
@@ -6,11 +6,8 @@ antigen use oh-my-zsh
 # Syntax highlighting bundle.
 antigen bundles <<EOBUNDLES
 git
-z
-vagrant
 extract
 tmux
-zsh-users/zsh-syntax-highlighting
 EOBUNDLES
 
 antigen theme $ZSH/prompt nope.zsh-theme
