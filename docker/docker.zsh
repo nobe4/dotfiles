d47f467f18882ac797551cbc846770ad06c3a49e
nobe4 <haffreinguev@gmail.com>
2016-12-12 12:39:57 +0000

    Simplify docker config, thanks to the new docker OS X App

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -1,17 +1,3 @@
-# Bypass docker
-return;
-
-# Check if docker exist before adding the env and aliases
-if ! hash docker 2>/dev/null; then
-  alias dk="echo 'Please install docker first'"
-  return
-fi
-
-if ! [ "$(docker-machine status)" = "Stopped" ]
-then
-  eval "$(docker-machine env default)"
-fi
-
 alias dk="docker"
 alias dc="docker-compose"
 
@@ -19,8 +5,6 @@ alias dkip="docker-machine ip default | /usr/bin/tr -d '\n' | tee >(pbcopy) | xa
 
 alias dkrmxt="docker rm \$(docker ps -q -f status=exited)"
 alias dkrmidg="docker images --all | awk '/^<none>/ {print $3}' | xargs docker rmi"
-
-alias dkenv="eval \"$(docker-machine env default)\""
 
 dksh(){
   docker exec -it $1 bash
