be06ac97982c88074ba5dfd92c1d149de2e69990
nobe4 <haffreinguev@gmail.com>
2016-12-14 15:07:07 +0000

    Update docker aliases

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -1,7 +1,6 @@
 alias dk="docker"
 alias dc="docker-compose"
-
-alias dkip="docker-machine ip default | /usr/bin/tr -d '\n' | tee >(pbcopy) | xargs echo copied$1"
+alias dcr="docker-compose run"
 
 alias dkrmxt="docker rm \$(docker ps -q -f status=exited)"
 alias dkrmidg="docker images --all | awk '/^<none>/ {print $3}' | xargs docker rmi"
