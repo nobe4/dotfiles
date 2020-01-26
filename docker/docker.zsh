bbbf1916a254109f4e6ebe5f6403879ab88c4f82
nobe4 <haffreinguev@gmail.com>
2020-01-26 16:12:10 +0100

    Add clean alias for docker

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -6,3 +6,5 @@ alias dkrmxt="docker rm \$(docker ps -q -f status=exited)"
 alias dkrmidg="docker images --all | awk '/^<none>/ {print $3}' | xargs docker rmi"
 
 alias bbox="docker run -it --rm busybox"
+
+alias dkclean="docker system prune --all --force --volumes"
