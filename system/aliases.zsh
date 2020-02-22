db6fe5a6035cb186e47d191f43b4573823d21958
nobe4 <haffreinguev@gmail.com>
2020-02-22 11:24:38 +0100

    Improve configuration for ubuntu

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -7,11 +7,13 @@ alias pg='ps aux | grep'
 # quicker kill all
 alias kk='killall'
 
-# RM Replacement
-# don't use rm unless you know what you're doing
-# to force rm, use full path : /bin/rm
-alias rm='rmtrash'
+if [ -n "$IS_MACOS" ]; then
+	# RM Replacement
+	# don't use rm unless you know what you're doing
+	# to force rm, use full path : /bin/rm
+	alias rm='rmtrash'
 
-# Mac Catalina is breaking everything, thanks Tim Apple.
-# from https://www.kvraudio.com/forum/viewtopic.php?t=530824&start=15
-alias unlockvst='xattr -d com.apple.quarantine'
+	# Mac Catalina is breaking everything, thanks Tim Apple.
+	# from https://www.kvraudio.com/forum/viewtopic.php?t=530824&start=15
+	alias unlockvst='xattr -d com.apple.quarantine'
+fi
