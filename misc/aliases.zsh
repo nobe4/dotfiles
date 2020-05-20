9ad230a8d9f8960906510f76bf22c2c8b61cb58d
nobe4 <haffreinguev@gmail.com>
2020-05-20 10:18:07 +0200

    Add ups function to track package

--- misc/aliases.zsh
+++ misc/aliases.zsh
@@ -5,3 +5,17 @@ function rssh(){
 alias sshr=rssh
 
 alias coffee="caffeinate -d"
+
+function ups() {
+	curl 'https://www.ups.com/track/api/Track/GetStatus?loc=en_US' \
+		-s \
+		-H 'Accept: application/json, text/plain, */*' \
+		--compressed \
+		-H 'Content-Type: application/json' \
+		--data-raw '{"Locale":"en_US","TrackingNumber":["'$1'"]}' \
+		| jq '.trackDetails[0].shipmentProgressActivities
+		| reverse
+		| .[]
+		| select(.activityScan != null)
+		| .date + " " + .time + " - " + .location + " - " + .activityScan'
+}
