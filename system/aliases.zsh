4069228478449d87d6081119e97c3a70228b1977
nobe4 <nobe4@users.noreply.github.com>
2021-05-06 20:54:26 +0200

    Fix rm alias

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -11,7 +11,7 @@ if [ -n "$IS_MACOS" ]; then
 	# RM Replacement
 	# don't use rm unless you know what you're doing
 	# to force rm, use full path : /bin/rm
-	alias rm='rmtrash'
+	alias rm='echo "use /bin/rm or trash"'
 
 	# Mac Catalina is breaking everything, thanks Tim Apple.
 	# from https://www.kvraudio.com/forum/viewtopic.php?t=530824&start=15
