22f7f3e0a1b7df5f221d0ec37bcce9d0c301688f
nobe4 <nobe4@users.noreply.github.com>
2021-05-06 18:36:52 +0200

    Cleanup after new install

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # dotfiles
 
 Kittens and stuff.
+
+# Install
+
+```
+git clone git@github.com:nobe4/dotfiles.git
+git submodule update --init
+chsh -s $(which zsh)
+```
+
+In case of `zsh compinit: insecure directories, run compaudit for list.`:
+
+```
+autoload -U compaudit
+compaudit | xargs chmod g-w
+```
--- search/fzf.zsh
+++ search/fzf.zsh
@@ -1,1 +1,12 @@
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+fi
+
+# Auto-completion
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
 export FZF_DEFAULT_COMMAND='rg --files --follow --ignore-file $ZSH/search/.ignore'
+
