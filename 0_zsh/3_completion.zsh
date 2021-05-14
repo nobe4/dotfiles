5749ea2dcdc6f4f0f442e18ea9fe66f899e08d7a
nobe4 <nobe4@users.noreply.github.com>
2021-05-14 11:27:42 +0200

    Put back some completion magic

--- 0_zsh/3_completion.zsh
+++ 0_zsh/3_completion.zsh
@@ -1,5 +1,9 @@
 autoload -U compinit && compinit
 zmodload -i zsh/complist
 
+# Enable completion from partial words
+# e.g. ~/men<TAB> => ~/Documents
+zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
 zstyle ':completion:*' list-colors
 zstyle ':completion:*' menu select
