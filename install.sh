df305a41fcd82ea413ee07e8395f211a3f54d754
nobe4 <nobe4@github.com>
2022-02-03 11:36:41 +0100

    Move private espanso config

--- install.sh
+++ install.sh
@@ -135,6 +135,7 @@ is_macos && {
 
 	# We're going to use the local espanso config so we can track its changes.
 	ln -fs "$DOTFILE_FOLDER/espanso_config.yml" "$HOME/Library/Preferences/espanso/default.yml"
+	ln -fs "$DOTFILE_FOLDER/private/espanso_config.yml" "$HOME/Library/Preferences/espanso/user/private.yml"
 }
 # }
 
