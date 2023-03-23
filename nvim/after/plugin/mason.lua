1d4c427164d2cffc8fc27bfffa8396351b115547
nobe4 <nobe4@users.noreply.github.com>
2023-03-23 19:01:27 +0100

    Automatically manage Mason package list

--- nvim/after/plugin/mason.lua
+++ nvim/after/plugin/mason.lua
@@ -1,7 +1,32 @@
-require("mason").setup({
+local mason = require("mason")
+local registry = require("mason-registry")
+local lsp_config = require("mason-lspconfig")
+
+mason.setup({
 	ui = {
 		border = "single",
 	},
 })
 
-require("mason-lspconfig").setup()
+local ensured_installed_packages = {
+	"golangci-lint",
+	"clang-format",
+	"vale",
+	"stylua",
+	"selene",
+	"clangd",
+	"goimports",
+	"gopls",
+	"prettier",
+	"solargraph",
+}
+
+for _, package in ipairs(ensured_installed_packages) do
+	local p = registry.get_package(package)
+
+	if not p:is_installed() then
+		p:install()
+	end
+end
+
+lsp_config.setup()
