2475fdf8b3a1d94527a987439e56bc5563fabe16
nobe4 <nobe4@users.noreply.github.com>
2023-03-28 18:32:55 +0200

    Change plugin manager to lazy.nvim

--- nvim/lua/nobe4/plugins.lua
+++ nvim/lua/nobe4/plugins.lua
@@ -1,105 +1,84 @@
 -- Bootstrap
-local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-local packer_bootstrap = false
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-	packer_bootstrap = vim.fn.system({
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
 		"git",
 		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
 	})
-	vim.cmd([[ packadd packer.nvim ]])
 end
+vim.opt.rtp:prepend(lazypath)
 
--- Compile on plugin change
-local packer_group = vim.api.nvim_create_augroup("Packer", { clear = true })
-vim.api.nvim_create_autocmd("BufWritePost", {
-	command = "source <afile> | PackerCompile",
-	group = packer_group,
-	pattern = "<afile>",
-})
-
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-	return
-end
-
--- Use a popup
-packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "single" })
-		end,
-		compact = true,
-		working_sym = ".",
-		error_sym = "!",
-		done_sym = "K",
-		removed_syn = "X",
-		moved_sym = "M",
-	},
-})
+local options = {
+	default = { lazy = true },
+	ui = { border = "rounded" },
+}
 
 -- Configs are in after/plugin
-return packer.startup(function(use)
-	use("wbthomason/packer.nvim") -- Packer can manage itself
-
-	use("~/Documents/dev/helldb")
+local plugins = {
+	-- Local
+	{ dir = "~/Documents/dev/helldb" },
 
 	-- Misc
-	use("nobe4/exchange.vim")
-	use("nobe4/curl.vim")
-	use("nobe4/vimcorrect")
-	use("tpope/vim-fugitive")
-	-- use("tpope/vim-projectionist")
-	use("tpope/vim-rhubarb")
-	use("tpope/vim-sleuth")
-	use("tpope/vim-surround")
-	-- use("tpope/vim-scriptease")
-	use("christoomey/vim-tmux-navigator")
-	use("danro/rename.vim")
-	use("editorconfig/editorconfig-vim")
-	use("junegunn/vim-easy-align")
-	use("mbbill/undotree")
-	use("romainl/vim-qf")
-	use("skywind3000/asyncrun.vim")
-	use("mattn/emmet-vim")
-	use("numToStr/Comment.nvim")
-	use("habamax/vim-godot")
+	"nobe4/exchange.vim",
+	"nobe4/curl.vim",
+	"nobe4/vimcorrect",
+	"tpope/vim-fugitive",
+	-- "tpope/vim-projectionist",
+	"tpope/vim-rhubarb",
+	"tpope/vim-sleuth",
+	"tpope/vim-surround",
+	-- "tpope/vim-scriptease",
+	"christoomey/vim-tmux-navigator",
+	"danro/rename.vim",
+	"editorconfig/editorconfig-vim",
+	"junegunn/vim-easy-align",
+	"mbbill/undotree",
+	"romainl/vim-qf",
+	"skywind3000/asyncrun.vim",
+	"mattn/emmet-vim",
+	"numToStr/Comment.nvim",
+	"habamax/vim-godot",
 
 	-- Treesitter
-	use("nvim-treesitter/nvim-treesitter")
-	use("nvim-treesitter/playground")
+	"nvim-treesitter/nvim-treesitter",
+	"nvim-treesitter/playground",
 
 	-- Snippet
-	use("L3MON4D3/LuaSnip")
+	"L3MON4D3/LuaSnip",
 
 	-- LSP
-	use("neovim/nvim-lspconfig")
+	"neovim/nvim-lspconfig",
 
 	-- Mason
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
 
 	-- Formatter
-	use({ "jose-elias-alvarez/null-ls.nvim", requires = { "nvim-lua/plenary.nvim" } })
+	{ "jose-elias-alvarez/null-ls.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
 
 	-- Fuzzy finder
-	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x", requires = { "nvim-lua/plenary.nvim" } })
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+	},
+	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 
 	-- Completion
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/cmp-nvim-lua")
+	"hrsh7th/nvim-cmp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-cmdline",
+	"hrsh7th/cmp-nvim-lua",
 
 	-- https://github.com/TimUntersberger/neogit
+}
 
-	if packer_bootstrap then
-		require("packer").sync()
-	end
-end)
+require("lazy").setup(plugins, options)
