8fb09b491722ef55252fd2edfed0d8e8fd164fa6
nobe4 <nobe4@users.noreply.github.com>
2023-11-01 21:22:20 +0100

    Switch to none-ls

--- nvim/lua/nobe4/plugins.lua
+++ nvim/lua/nobe4/plugins.lua
@@ -57,7 +57,7 @@ local plugins = {
 	"williamboman/mason-lspconfig.nvim",
 
 	-- Formatter
-	{ "jose-elias-alvarez/null-ls.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+	{ "nvimtools/none-ls.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
 
 	-- Fuzzy finder
 	{
