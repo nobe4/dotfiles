f6117ed0a9683018181c444c8f8a889ed75ee2d7
nobe4 <nobe4@users.noreply.github.com>
2023-11-02 23:56:40 +0100

    Configure trouble

---
+++ nvim/after/plugin/trouble.lua
@@ -0,0 +1,14 @@
+require("trouble").setup({
+	icons = false,
+	padding = false,
+	fold_open = "v",
+	fold_close = ">",
+	indent_line = false,
+	signs = {
+		error = "E",
+		warning = "W",
+		hint = "H",
+		information = "I",
+		other = "O",
+	},
+})
--- nvim/lua/nobe4/mappings.lua
+++ nvim/lua/nobe4/mappings.lua
@@ -35,28 +35,28 @@ map("n", "<Leader>w", ":noautocmd w<CR>", options) -- Save file without autocmd
 map("n", "<Leader>q", ":quit!", options)
 map("n", "<Leader>x", ":xit", options)
 map("n", "<Leader>m", ":make", options)
-map("n", "<Leader>R", ":nnoremap <lt>Leader>r :", options)                                              -- Prepare a quick command: http://vi.stackexchange.com/a/3136/1821
-map("n", "<Leader>r", ":<UP>", options)                                                                 -- repeat last command
+map("n", "<Leader>R", ":nnoremap <lt>Leader>r :", options) -- Prepare a quick command: http://vi.stackexchange.com/a/3136/1821
+map("n", "<Leader>r", ":<UP>", options) -- repeat last command
 
-map("n", "go", ":call system('open ' . expand('<cWORD>'))<CR>", options)                                -- open WORD under cursor
-map("n", "gf", ":e <cfile><CR>", options)                                                               -- open file under cursor
+map("n", "go", ":call system('open ' . expand('<cWORD>'))<CR>", options) -- open WORD under cursor
+map("n", "gf", ":e <cfile><CR>", options) -- open file under cursor
 
-map("n", "<Leader>b", ":b#<CR>", options)                                                               -- show buffer list
-map("n", "<Leader>l", ":ls<CR>:buffer<Space>", options)                                                 -- show buffers and wait for a selection
+map("n", "<Leader>b", ":b#<CR>", options) -- show buffer list
+map("n", "<Leader>l", ":ls<CR>:buffer<Space>", options) -- show buffers and wait for a selection
 map("n", "<Leader><Leader>", ":<C-U>nohlsearch<C-R>=has('diff')?'<Bar>diffupdate':''<CR><CR>", options) -- clear highlight
-map("n", '"p', [[:reg <bar> exec 'normal!"'.input('>').'p'<CR>]], options)                              -- show registers value before pasting
+map("n", '"p', [[:reg <bar> exec 'normal!"'.input('>').'p'<CR>]], options) -- show registers value before pasting
 
-map("c", "<C-A>", "<Home>", options)                                                                    -- Go to the start of the command line
+map("c", "<C-A>", "<Home>", options) -- Go to the start of the command line
 
 -- Add edition breakpoint on different keypress
 map("i", "<CR>", "<C-G>u<CR>", options)
 map("i", "<C-U>", "<C-G>u<C-U>", options)
 map("i", "<C-W>", "<C-G>u<C-W>", options)
 
-map("c", "<C-F>", "<C-R>=expand('%:p:h')<CR>/", options)                    -- Insert path to current file
-map("n", "<Leader>e", ":edit <C-R>=expand('%:p:h')<CR>/", options)          -- Prepare to edit a file in the same folder as the current one
+map("c", "<C-F>", "<C-R>=expand('%:p:h')<CR>/", options) -- Insert path to current file
+map("n", "<Leader>e", ":edit <C-R>=expand('%:p:h')<CR>/", options) -- Prepare to edit a file in the same folder as the current one
 map("n", "<Leader>z", ":execute 'tabnew +' . line('.') .' %'<CR>", options) -- Zoom in current buffer
-map("t", "<Esc>", [[<C-\><C-n>]], options)                                  -- Escape in terminal
+map("t", "<Esc>", [[<C-\><C-n>]], options) -- Escape in terminal
 
 -- Fugitive
 map("n", "<Leader>gs", ":tabnew +Git status<CR>", options)
@@ -93,6 +93,10 @@ map_braquet("l", "l") -- jump between lines in the (l)ocation list
 map("n", "]e", vim.diagnostic.goto_next, options)
 map("n", "[e", vim.diagnostic.goto_prev, options)
 map("n", "[o", vim.diagnostic.open_float, options)
+
+map("n", "T", function()
+	require("trouble").toggle()
+end, options)
 
 -- vim-tmux-navigator
 vim.g.tmux_navigator_no_mappings = 1
