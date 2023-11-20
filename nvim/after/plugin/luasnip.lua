568ecc8894d9c40861167a1c02825e471b599304
nobe4 <nobe4@users.noreply.github.com>
2023-11-20 23:25:33 +0100

    fix(luasnip): fix the gitcommit snippet
    Also add modeline snippet for all files as a POC

--- nvim/after/plugin/luasnip.lua
+++ nvim/after/plugin/luasnip.lua
@@ -7,95 +7,104 @@ local ms = ls.multi_snippet
 local fmt = require("luasnip.extras.fmt").fmt
 
 ls.config.setup({
-	history = true,
-	updateevents = "TextChanged,TextChangedI",
-	enable_autosnippets = true,
+  history = true,
+  updateevents = "TextChanged,TextChangedI",
+  enable_autosnippets = true,
 })
 
 ls.add_snippets("all", {
-	s("modeline", {
-		t("#!/usr/bin/env "),
-		i(1, "interpreter"),
-	}),
+  s("modeline", {
+    t("#!/usr/bin/env "),
+    i(1, "interpreter"),
+  }),
 })
 
 ls.add_snippets("ruby", {
-	ls.parser.parse_snippet(
-		"describe",
-		[[
+  ls.parser.parse_snippet(
+    "describe",
+    [[
 describe "#$1" do
   it "$2" do
     $3
   end
 end]]
-	),
+  ),
 
-	ls.parser.parse_snippet(
-		"before",
-		[[
+  ls.parser.parse_snippet(
+    "before",
+    [[
 before do
   $1
 end]]
-	),
+  ),
 
-	ls.parser.parse_snippet(
-		"context",
-		[[
+  ls.parser.parse_snippet(
+    "context",
+    [[
 context "$1" do
   it "$2" do
     $3
   end
 end]]
-	),
+  ),
 
-	-- TODO: have a choice between the `let` and the `X =`
-	ls.parser.parse_snippet("let", [[let(:$1) { "$1" }]]),
-	ls.parser.parse_snippet("double", [[let(:$1) { double("$1") }]]),
-	ls.parser.parse_snippet("pry", [[require "pry-byebug"; binding.pry]]),
-	ls.parser.parse_snippet("freeze", "# frozen_string_literal: true"),
+  -- TODO: have a choice between the `let` and the `X =`
+  ls.parser.parse_snippet("let", [[let(:$1) { "$1" }]]),
+  ls.parser.parse_snippet("double", [[let(:$1) { double("$1") }]]),
+  ls.parser.parse_snippet("pry", [[require "pry-byebug"; binding.pry]]),
+  ls.parser.parse_snippet("freeze", "# frozen_string_literal: true"),
 })
 
 ls.add_snippets("go", {
-	ls.parser.parse_snippet("test", 'func Test${1:Name}(t *testing.T) {${2:t.Skip("TODO")}}'),
-	ls.parser.parse_snippet("run", 't.Run("$1", func(t *testing.T) {${2:t.Skip("TODO")}})'),
+  ls.parser.parse_snippet("test", 'func Test${1:Name}(t *testing.T) {${2:t.Skip("TODO")}}'),
+  ls.parser.parse_snippet("run", 't.Run("$1", func(t *testing.T) {${2:t.Skip("TODO")}})'),
 })
 
 ls.add_snippets("gitcommit", {
-	ls.parser.parse_snippet("missing", "Remove missing team"),
-	ls.parser.parse_snippet("empty", "Remove empty team"),
-	ls.parser.parse_snippet("checksums", "Rebuild checksums"),
-	-- https://www.conventionalcommits.org/en/v1.0.0/#specification
-	ms({ "feat", "fix", "docs", "refactor", "test" }, {
-		fmt([[ {}({}):{} {} ]], {
-			f(function(_, parent)
-				return parent.trigger
-			end, {}),
-			i(1, "reference"),
-			i(2, "title"),
-			i(3, "body"),
-		}),
-	}),
+  ls.parser.parse_snippet("missing", "Remove missing team"),
+  ls.parser.parse_snippet("empty", "Remove empty team"),
+  ls.parser.parse_snippet("checksums", "Rebuild checksums"),
+
+  -- https://www.conventionalcommits.org/en/v1.0.0/#specification
+  ms(
+    { "feat", "fix", "docs", "refactor", "test" },
+    fmt(
+      [[
+      {}({}):{}
+
+      {}
+      ]],
+      {
+        f(function(_, parent)
+          return parent.trigger
+        end, {}),
+        i(1, "reference"),
+        i(2, "title"),
+        i(3, "body"),
+      }
+    )
+  ),
 })
 
 ls.add_snippets("sh", {
-	s("shellcheck ignore", { t("# shellcheck disable=${1:code}") }),
+  s("shellcheck ignore", { t("# shellcheck disable=${1:code}") }),
 })
 
 -- mappings
 vim.keymap.set({ "i", "s" }, "<C-K>", function()
-	if ls.expand_or_jumpable() then
-		ls.expand_or_jump()
-	end
+  if ls.expand_or_jumpable() then
+    ls.expand_or_jump()
+  end
 end, { silent = true })
 
 vim.keymap.set({ "i", "s" }, "<C-J>", function()
-	if ls.jumpable(-1) then
-		ls.jump(-1)
-	end
+  if ls.jumpable(-1) then
+    ls.jump(-1)
+  end
 end, { silent = true })
 
 vim.keymap.set({ "i", "s" }, "<C-L>", function()
-	if ls.choice_active() then
-		ls.change_choice(1)
-	end
+  if ls.choice_active() then
+    ls.change_choice(1)
+  end
 end)
