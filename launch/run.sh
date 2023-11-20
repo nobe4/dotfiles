1fab2677dfbe60f21e643464438d62b7d8b0a6cd
nobe4 <nobe4@users.noreply.github.com>
2023-11-21 00:41:57 +0100

    docs(launch): simplify slightly the needed command in automator
    This assumes that ~/.zprofile sets up the path correctly to all the
    binaries, which should be the case anyway.

--- launch/run.sh
+++ launch/run.sh
@@ -3,13 +3,10 @@
 # Setup:
 # 1. create an automator workflow that runs a bash script
 #
-#   source /Users/n/dev/nobe4/dotfiles/.zprofile && \
-#   /Applications/kitty.app/Contents/MacOS/kitty \
-#     --config "${DOTFILE_FOLDER}/launch/kitty.conf" \
-#     "${DOTFILE_FOLDER}/launch/run.sh"
+#  source "${HOME}/.zprofile" && \
+#  kitty --config "${DOTFILE_FOLDER}/launch/kitty.conf" "${DOTFILE_FOLDER}/launch/run.sh"
 #
-# 2. Settings > Keyboard > Shortcuts
-#    > Services > General > Set the created service
+# 2. Settings > Keyboard > Shortcuts > Services > General > Set the service
 
 PREVIEW_COMMAND="grep ^#/ <${DOTFILE_FOLDER}/launch/commands/{} | cut -c4- | envsubst"
 COMMANDS_PATH="${DOTFILE_FOLDER}/launch/commands"
