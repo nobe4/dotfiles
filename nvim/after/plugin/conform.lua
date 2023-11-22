82f0827932f825638da7d86fbac83274969c7898
nobe4 <nobe4@users.noreply.github.com>
2023-11-22 14:11:22 +0100

    fix(format): use lua_ls for formatting
    Remove stylua from mason

--- nvim/after/plugin/conform.lua
+++ nvim/after/plugin/conform.lua
@@ -1,28 +1,27 @@
 require("conform").setup({
-	format_on_save = {
-		timeout_ms = 500,
-		lsp_fallback = true,
-	},
-	formatters_by_ft = {
-		c = { "clang_format" },
-		cpp = { "clang_format" },
-		lua = { "stylua" },
-		ruby = { "rubocop" },
-		python = { "black" },
-		golang = { "gofmt", "goimports" },
-		javascript = { "prettier" },
-		html = { "prettier" },
-		css = { "prettier" },
-		graphql = { "prettier" },
-		json = { "prettier" },
-		-- TODO: add for lua
-		-- formatting.yamlfmt.with({
-		-- 	extra_args = { "--formatter", "indentless_arrays=true,retain_line_breaks=true" },
-		-- }),
-	},
-	formatters = {
-		rubocop = {
-			command = require("nobe4.tool.rubocop").command(),
-		},
-	},
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
+  formatters_by_ft = {
+    c = { "clang_format" },
+    cpp = { "clang_format" },
+    ruby = { "rubocop" },
+    python = { "black" },
+    golang = { "gofmt", "goimports" },
+    javascript = { "prettier" },
+    html = { "prettier" },
+    css = { "prettier" },
+    graphql = { "prettier" },
+    json = { "prettier" },
+    -- TODO: add for yaml
+    -- formatting.yamlfmt.with({
+    -- 	extra_args = { "--formatter", "indentless_arrays=true,retain_line_breaks=true" },
+    -- }),
+  },
+  formatters = {
+    rubocop = {
+      command = require("nobe4.tool.rubocop").command(),
+    },
+  },
 })
