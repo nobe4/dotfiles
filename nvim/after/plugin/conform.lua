f780220f5c137521760c1ab54980195ef1b8970b
nobe4 <nobe4@users.noreply.github.com>
2024-01-17 23:51:35 +0100

    fix(conform): use correct type for go files

--- nvim/after/plugin/conform.lua
+++ nvim/after/plugin/conform.lua
@@ -1,18 +1,18 @@
 require("conform").setup({
-  format_on_save = {
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
-  formatters_by_ft = {
-    c = { "clang_format" },
-    cpp = { "clang_format" },
-    python = { "black" },
-    golang = { "gofmt", "goimports" },
-    javascript = { "prettier" },
-    graphql = { "prettier" },
-    -- TODO: add for yaml
-    -- formatting.yamlfmt.with({
-    -- 	extra_args = { "--formatter", "indentless_arrays=true,retain_line_breaks=true" },
-    -- }),
-  },
+	format_on_save = {
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
+	formatters_by_ft = {
+		c = { "clang_format" },
+		cpp = { "clang_format" },
+		python = { "black" },
+		go = { "goimports", "gofmt" },
+		javascript = { "prettier" },
+		graphql = { "prettier" },
+		-- TODO: add for yaml
+		-- formatting.yamlfmt.with({
+		-- 	extra_args = { "--formatter", "indentless_arrays=true,retain_line_breaks=true" },
+		-- }),
+	},
 })
