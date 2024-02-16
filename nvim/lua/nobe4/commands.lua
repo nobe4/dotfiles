c88d95f004a07c3a7cfee84d9c90051d57531451
nobe4 <nobe4@users.noreply.github.com>
2024-02-16 09:32:02 +0100

    feat: add Browse command to fix GBrowse that stopped working
    disabling netrw broke fugitive
    
    see  https://vi.stackexchange.com/a/43348

--- nvim/lua/nobe4/commands.lua
+++ nvim/lua/nobe4/commands.lua
@@ -29,9 +29,9 @@ command("Mouse", function()
 	]])
 end, opts)
 
-command("Format", function()
-	vim.lsp.buf.formatting()
-end, opts)
+command("Format", function() vim.lsp.buf.formatting() end, opts)
+command("Browse", function(o) vim.fn.system { "open", o.fargs[1] } end, { nargs = 1, bang = true })
+
 command("ColorGroup", function()
 	vim.cmd([[
 		let s = synID(line('.'), col('.'), 1) | echo synIDattr(s, 'name') . ' -> ' . synIDattr(synIDtrans(s), 'name')
@@ -105,11 +105,11 @@ M.telescope = function(ts_builtin)
 end
 
 if vim.g.neovide then
-	vim.keymap.set("n", "<D-s>", ":w<CR>") -- Save
-	vim.keymap.set("v", "<D-c>", '"+y') -- Copy
-	vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
-	vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
-	vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
+	vim.keymap.set("n", "<D-s>", ":w<CR>")   -- Save
+	vim.keymap.set("v", "<D-c>", '"+y')      -- Copy
+	vim.keymap.set("n", "<D-v>", '"+P')      -- Paste normal mode
+	vim.keymap.set("v", "<D-v>", '"+P')      -- Paste visual mode
+	vim.keymap.set("c", "<D-v>", "<C-R>+")   -- Paste command mode
 	vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
 	-- Allow clipboard copy paste in neovim
 	vim.api.nvim_set_keymap("", "<D-v>", "+p<CR>", { noremap = true, silent = true })
