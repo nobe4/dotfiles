8aa0fa98a584c242ecac03de727d8f4a6272fd82
nobe4 <nobe4@users.noreply.github.com>
2024-03-18 09:54:35 +0100

    fix: create config dir but link to dotfile dir

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,10 @@
 set -ex
 
 DOTFILES_SOURCE="${HOME}/dev/nobe4/dotfiles"
-DOTFILES_DIR="${HOME}/.config/dotfiles"
+CONFIG_DIR="${HOME}/.config"
+DOTFILES_DIR="${CONFIG_DIR}/dotfiles"
 
-mkdir -p "${DOTFILES_SOURCE}" "${DOTFILES_DIR}"
+mkdir -p "${DOTFILES_SOURCE}" "${CONFIG_DIR}"
 
 git clone --recurse-submodules git@github.com:nobe4/dotfiles.git "${DOTFILES_SOURCE}"
 ln -fs "${DOTFILES_SOURCE}" "${DOTFILES_DIR}"
