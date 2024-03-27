cba7873deaea0058d3f70099874854e1ba7238e2
nobe4 <nobe4@users.noreply.github.com>
2024-03-27 09:33:27 +0100

    fix: improve sling run

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,6 +1,19 @@
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
-hs.hotkey.bind({"shift"}, "space", function()
-    io.popen('source "${HOME}/.zprofile" && kitty sling')
+hs.hotkey.bind({ "shift" }, "space", function()
+	-- create a new kitty OS window
+	local app = hs.application.get("kitty")
+	if app then
+		app:selectMenuItem({ "Shell", "New OS Window" })
+	else
+		hs.application.launchOrFocus("kitty")
+	end
+	app = hs.application.get("kitty")
+
+	app:activate()
+	app:focusedWindow():centerOnScreen(nil, true)
+
+	hs.eventtap.keyStrokes("clear ; sling ; kitty @ close-window --self", app)
+	hs.eventtap.keyStroke({}, "return")
 end)
