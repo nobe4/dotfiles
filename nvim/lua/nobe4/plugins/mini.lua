8b85a6340f64675cec3b52497c9b47486e5fb7c2
nobe4 <nobe4@users.noreply.github.com>
2025-06-06 19:25:14 +0200

    fix(surrount): require `g` to surround
    I use `s` a lot, so this is better, and makes it more consistent with
    other custom mappings.

--- nvim/lua/nobe4/plugins/mini.lua
+++ nvim/lua/nobe4/plugins/mini.lua
@@ -3,7 +3,21 @@ return {
 		"echasnovski/mini.nvim",
 		config = function()
 			require("mini.ai").setup()
-			require("mini.surround").setup()
+			require("mini.surround").setup({
+				mappings = {
+					add = "gsa",
+					delete = "gsd",
+					find = "gsf",
+					find_left = "gsF",
+					highlight = "gsh",
+					replace = "gsr",
+					update_n_lines = "gsn",
+
+					suffix_last = "l",
+					suffix_next = "n",
+				},
+
+			})
 			require("mini.align").setup()
 		end,
 	},
