bde8ee685fcf46a7f4e48ac8aa92084aafd319aa
nobe4 <nobe4@users.noreply.github.com>
2026-02-24 19:08:37 +0100

    feat(copy): showcase using writeShellApplication
    It adds shellcheck and offers inputs management.

--- nixos/packages/copy.nix
+++ nixos/packages/copy.nix
@@ -1,16 +1,20 @@
 { config, pkgs, ... }:
-pkgs.writeShellScriptBin "copy" (
-  if pkgs.stdenv.isDarwin then
-    ''
-      pbcopy
-    ''
-  else if config.programs.hyprland.enable then
-    ''
-      ${pkgs.wl-clipboard}/bin/wl-copy
-    ''
-  else
-    ''
+if pkgs.stdenv.isDarwin then
+  pkgs.writeShellApplication {
+    name = "copy";
+    text = "pbcopy";
+  }
+else if config.programs.hyprland.enable then
+  pkgs.writeShellApplication {
+    name = "copy";
+    runtimeInputs = [ pkgs.wl-clipboard ];
+    text = "wl-copy";
+  }
+else
+  pkgs.writeShellApplication {
+    name = "copy";
+    text = ''
       echo "copy not supported"
       exit 1
-    ''
-)
+    '';
+  }
