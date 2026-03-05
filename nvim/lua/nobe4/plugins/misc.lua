fdeb5432bb8c151e644ddb1af15a05c53d2473f2
nobe4 <nobe4@users.noreply.github.com>
2026-03-05 10:41:46 +0100

    chore(nvim): cleanup unused plugins
    Sidekick offers little more than copilot on a different pane, and the
    view is buggy.

--- nvim/lua/nobe4/plugins/misc.lua
+++ nvim/lua/nobe4/plugins/misc.lua
@@ -4,6 +4,5 @@ return {
 	"romainl/vim-qf",
 
 	"nvim-tree/nvim-web-devicons",
-	{ "norcalli/nvim-colorizer.lua", config = function() require("colorizer").setup() end },
 	{ "hat0uma/csvview.nvim",        config = function() require("csvview").setup() end },
 }
