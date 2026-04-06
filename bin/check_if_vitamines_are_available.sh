b36370c35a5a23287f60247d273645899d41554a
nobe4 <nobe4@users.noreply.github.com>
2026-04-06 13:08:36 +0200

    feat(check_if_vitamines_are_available): always notify
    Otherwise I'll forget that this thing is even running.

--- bin/check_if_vitamines_are_available.sh
+++ bin/check_if_vitamines_are_available.sh
@@ -1,6 +1,5 @@
 if curl --silent https://watsonnutrition.de/products/male-one | grep 'price__badge-sold-out' > /dev/null; then
-	# pass
-	true
+	notify -t 1000 "Vitamines are not available!"
 else
 	notify "Vitamines are available!" "Go to https://watsonnutrition.de/products/male-one"
 fi
