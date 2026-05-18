e591b547747d3817972b64a1fa7f090723f3ab2c
nobe4 <nobe4@users.noreply.github.com>
2026-05-18 17:06:22 +0200

    fix(treesitter): fix config after upgrading nvim to 0.12

--- nvim/lua/nobe4/plugins/treesitter.lua
+++ nvim/lua/nobe4/plugins/treesitter.lua
@@ -1,46 +1,38 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
-		dependencies = { "nvim-treesitter/playground" },
+		branch = "main",
+		-- dependencies = { "nvim-treesitter/playground" },
 		build = ":TSUpdate",
 
 		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = {
-					"c", "cpp",
-					"vim", "vimdoc",
-					"lua", "ruby", "go",
-					"html", "css", "javascript",
-					"markdown",
-					"markdown_inline",
-					"comment",
-				},
+			local ts = require("nvim-treesitter")
+			ts.install({
+				"c", "cpp",
+				"vim", "vimdoc",
+				"lua", "ruby", "go",
+				"html", "css", "javascript",
+				"markdown",
+				"markdown_inline",
+				"comment",
+			})
+			vim.api.nvim_create_autocmd('FileType', {
+				pattern = { '<filetype>' },
+				callback = function()
+					vim.treesitter.start()
+					vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+					vim.wo[0][0].foldmethod = 'expr'
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 
-				auto_install = true,
-				sync_install = false,
-
-				indent = { enable = true },
-
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
-
-				playground = { enable = true },
-
-				query_linter = {
-					enable = true,
-					use_virtual_text = true,
-					lint_events = { "BufWrite", "CursorHold" },
-				},
+				end,
 			})
 		end,
 	},
 
-	{
-		"nvim-treesitter/nvim-treesitter-context",
-		config = function()
-			require("treesitter-context").setup({ separator = "-" })
-		end,
-	},
+	-- {
+	-- 	"nvim-treesitter/nvim-treesitter-context",
+	-- 	config = function()
+	-- 		require("treesitter-context").setup({ separator = "-" })
+	-- 	end,
+	-- },
 }
