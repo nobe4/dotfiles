0dea6bbba58868dba99ca1a3372cb4bdb0429c45
nobe4 <nobe4@users.noreply.github.com>
2026-06-25 08:06:49 +0200

    fix(bootstrap): post worktree migration

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,19 @@
 
 set -ex
 
-DOTFILES_SOURCE="${HOME}/dev/nobe4/dotfiles"
+DOTFILES_REPO="git@github.com:nobe4/dotfiles.git"
+DOTFILES_DIR="${HOME}/dev/nobe4/dotfiles"
+DOTFILES_SOURCE="${DOTFILES_DIR}/worktree/main"
 CONFIG_DIR="${HOME}/.config"
 DOTFILES_DIR="${CONFIG_DIR}/dotfiles"
 
 mkdir -p "${DOTFILES_SOURCE}" "${CONFIG_DIR}"
 
-git clone --recurse-submodules git@github.com:nobe4/dotfiles.git "${DOTFILES_SOURCE}"
+git clone "${DOTFILES_REPO}" "${DOTFILES_DIR}" --bare --config=remote.origin.fetch='+refs/heads/*:refs/remotes/origin/*'
+git -C "${DOTFILES_DIR}" fetch origin
+git -C "${DOTFILES_DIR}" worktree add "worktree/main" "main"
+git -C "${DOTFILES_SOURCE}" submodule update --recursive --remote
+
 ln -fs "${DOTFILES_SOURCE}" "${DOTFILES_DIR}"
 
 echo "Now run"
