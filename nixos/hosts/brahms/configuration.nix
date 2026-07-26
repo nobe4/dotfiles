615b22eb4d86f05b7053acb75ed8208a6c7d60d6
nobe4 <nobe4@users.noreply.github.com>
2026-07-26 10:07:20 +0200

    feat(brahms): backup macos config
    Brahms is now a nixos machine 🎉
    https://nobe4.fr/posts/nixos-on-an-intel-macbook/

--- nixos/hosts/brahms/configuration.nix
+++ nixos/hosts/brahms/configuration.nix
@@ -1,139 +1,70 @@
-{ config, pkgs, ... }:
 {
-  environment.darwinConfig = "${config.dotfiles}/nixos/hosts/brahms/configuration.nix";
-
-  # XXX: https://github.com/nix-darwin/nix-darwin/issues/1695
-  disabledModules = [
-    "programs/zsh"
-    "programs/bash"
-  ];
-
+  pkgs,
+  ...
+}:
+let
+  keys = import ../../packages/ssh_keys.nix;
+in
+{
   imports = [
-    # XXX: https://github.com/nix-darwin/nix-darwin/issues/1695
-    ../../modules/darwin/zsh.nix
-    ../../modules/darwin/bash.nix
+    ./hardware-configuration.nix
+    "${builtins.fetchGit { url = "https://github.com/NixOS/nixos-hardware.git"; }}/apple/t2"
 
-    ../../utils/allowed_unfree.nix
-    ../../utils/ln-darwin.nix
+    ../../utils/ln.nix
 
     ../../users/nobe4.nix
-
-    ../../packages/shell
-    ../../packages/system.nix
-    ../../packages/1password.nix
-    ../../packages/nix
-    ../../packages/dev.nix
-
-    ./shortcuts
   ];
 
-  networking = {
-    computerName = "brahms";
-    hostName = "brahms";
-    localHostName = "brahms";
-  };
-
-  users.users.nobe4.packages = with pkgs; [
-    darwin.trash
-    gnupg
+  # TODO: move to a module with the import.
+  # macos t2 specific
+  hardware.firmware = [
+    (pkgs.stdenvNoCC.mkDerivation (final: {
+      name = "brcm-firmware";
+      src = ./firmware.tar;
+      dontUnpack = true;
+      installPhase = ''
+        mkdir -p $out/lib/firmware/brcm
+        tar -xf ${final.src} -C $out/lib/firmware/brcm
+      '';
+    }))
   ];
 
-  homebrew = {
-    # Following https://github.com/nix-darwin/nix-darwin?tab=readme-ov-file
-    # For brew
-    #     https://github.com/orgs/Homebrew/discussions/3227
-    #     `sudo chown -R $(whoami) /usr/local/share/zsh/site-functions`
-    #     + install brew from https://brew.sh/
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
 
-    enable = true;
-    brews = [
-      "pinentry-mac"
-    ];
-
-    # TODO: check what can be removed here
-    casks = [
-      "firefox"
-      # "1password"
-      # "1password-cli"
-      "kitty"
-      "notunes"
-      "hammerspoon"
-      "neovide"
-    ];
+  # Prevent a nixos-switch to stop those services, loosing connections from an
+  # ssh build.
+  systemd.services = {
+    NetworkManager.stopIfChanged = false;
+    dbus-broker.stopIfChanged = false;
   };
 
-  ln = with config; [
-    [
-      "${dotfiles}/hammerspoon/"
-      "${home}/.hammerspoon"
-    ]
+  # TODO: end move
+
+  users.users.nobe4 = {
+    openssh.authorizedKeys.keys = [ keys.nobe4_verdi ];
+  };
+
+  # Configure network connections interactively with nmcli or nmtui.
+  networking.networkmanager.enable = true;
+
+  networking.hostName = "brahms";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console.keyMap = "colemak";
+
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    git
+    htop
   ];
 
-  system = {
-    primaryUser = "nobe4";
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
 
-    defaults = {
-      NSGlobalDomain = {
-        AppleInterfaceStyle = "Dark";
-        AppleShowAllExtensions = true;
-        AppleShowAllFiles = true;
-        AppleEnableMouseSwipeNavigateWithScrolls = false;
-        AppleEnableSwipeNavigateWithScrolls = false;
-        "com.apple.sound.beep.feedback" = 1;
-        # Not working?
-        "com.apple.swipescrolldirection" = false;
-      };
-      controlcenter = {
-        BatteryShowPercentage = true;
-        Bluetooth = false;
-        Display = false;
-      };
-      dock = {
-        autohide = true;
-        static-only = true;
-      };
-      finder = {
-        AppleShowAllExtensions = true;
-        AppleShowAllFiles = true;
-        FXEnableExtensionChangeWarning = false;
-        FXPreferredViewStyle = "clmv";
-        NewWindowTarget = "Home";
-        ShowPathbar = true;
-        _FXShowPosixPathInTitle = true;
-      };
-      hitoolbox = {
-        AppleFnUsageType = "Change Input Source";
-      };
-      loginwindow = {
-        DisableConsoleAccess = true;
-        GuestEnabled = false;
-      };
-      trackpad = {
-        Clicking = false;
-      };
-      ".GlobalPreferences" = {
-        "com.apple.mouse.scaling" = 5.0;
-      };
-    };
-
-    keyboard = {
-      enableKeyMapping = true;
-      remapCapsLockToEscape = true;
-
-      shortcuts = {
-        enable = true;
-        disableAll = true;
-
-        binds = {
-          Spotlight.search = {
-            enabled = true;
-          };
-        };
-      };
-    };
-  };
-
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
-  system.stateVersion = 6;
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "26.11"; # Did you read the comment?
 }
---
+++ nixos/hosts/brahms_macos_backup/configuration.nix
@@ -0,0 +1,139 @@
+{ config, pkgs, ... }:
+{
+  environment.darwinConfig = "${config.dotfiles}/nixos/hosts/brahms/configuration.nix";
+
+  # XXX: https://github.com/nix-darwin/nix-darwin/issues/1695
+  disabledModules = [
+    "programs/zsh"
+    "programs/bash"
+  ];
+
+  imports = [
+    # XXX: https://github.com/nix-darwin/nix-darwin/issues/1695
+    ../../modules/darwin/zsh.nix
+    ../../modules/darwin/bash.nix
+
+    ../../utils/allowed_unfree.nix
+    ../../utils/ln-darwin.nix
+
+    ../../users/nobe4.nix
+
+    ../../packages/shell
+    ../../packages/system.nix
+    ../../packages/1password.nix
+    ../../packages/nix
+    ../../packages/dev.nix
+
+    ./shortcuts
+  ];
+
+  networking = {
+    computerName = "brahms";
+    hostName = "brahms";
+    localHostName = "brahms";
+  };
+
+  users.users.nobe4.packages = with pkgs; [
+    darwin.trash
+    gnupg
+  ];
+
+  homebrew = {
+    # Following https://github.com/nix-darwin/nix-darwin?tab=readme-ov-file
+    # For brew
+    #     https://github.com/orgs/Homebrew/discussions/3227
+    #     `sudo chown -R $(whoami) /usr/local/share/zsh/site-functions`
+    #     + install brew from https://brew.sh/
+
+    enable = true;
+    brews = [
+      "pinentry-mac"
+    ];
+
+    # TODO: check what can be removed here
+    casks = [
+      "firefox"
+      # "1password"
+      # "1password-cli"
+      "kitty"
+      "notunes"
+      "hammerspoon"
+      "neovide"
+    ];
+  };
+
+  ln = with config; [
+    [
+      "${dotfiles}/hammerspoon/"
+      "${home}/.hammerspoon"
+    ]
+  ];
+
+  system = {
+    primaryUser = "nobe4";
+
+    defaults = {
+      NSGlobalDomain = {
+        AppleInterfaceStyle = "Dark";
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        AppleEnableMouseSwipeNavigateWithScrolls = false;
+        AppleEnableSwipeNavigateWithScrolls = false;
+        "com.apple.sound.beep.feedback" = 1;
+        # Not working?
+        "com.apple.swipescrolldirection" = false;
+      };
+      controlcenter = {
+        BatteryShowPercentage = true;
+        Bluetooth = false;
+        Display = false;
+      };
+      dock = {
+        autohide = true;
+        static-only = true;
+      };
+      finder = {
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        FXEnableExtensionChangeWarning = false;
+        FXPreferredViewStyle = "clmv";
+        NewWindowTarget = "Home";
+        ShowPathbar = true;
+        _FXShowPosixPathInTitle = true;
+      };
+      hitoolbox = {
+        AppleFnUsageType = "Change Input Source";
+      };
+      loginwindow = {
+        DisableConsoleAccess = true;
+        GuestEnabled = false;
+      };
+      trackpad = {
+        Clicking = false;
+      };
+      ".GlobalPreferences" = {
+        "com.apple.mouse.scaling" = 5.0;
+      };
+    };
+
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToEscape = true;
+
+      shortcuts = {
+        enable = true;
+        disableAll = true;
+
+        binds = {
+          Spotlight.search = {
+            enabled = true;
+          };
+        };
+      };
+    };
+  };
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 6;
+}
