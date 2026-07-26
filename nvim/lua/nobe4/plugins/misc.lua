d7001b7da3dec0ad2944bc2265e589635141687e
nobe4 <nobe4@users.noreply.github.com>
2026-07-26 10:09:01 +0200

    feat(nvim): install zen plugin for calmer prose writing

--- nvim/lua/nobe4/plugins/misc.lua
+++ nvim/lua/nobe4/plugins/misc.lua
@@ -2,6 +2,16 @@ return {
 	"mbbill/undotree",
 	"danro/rename.vim",
 	"romainl/vim-qf",
+	{
+		"folke/zen-mode.nvim",
+		opts = {
+			window = {
+				-- Text width + some buffer
+				width = 85,
+			},
+		},
+	},
+
 
 	"nvim-tree/nvim-web-devicons",
 	{ "norcalli/nvim-colorizer.lua", config = function() require("colorizer").setup() end },
