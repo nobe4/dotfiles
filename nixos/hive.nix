985939cc3dbfb70e4dd763d3b3b650ae1ebac1b6
nobe4 <nobe4@users.noreply.github.com>
2026-07-29 12:50:14 +0200

    feat(verdi): update to 26.05
    Also fix some hyprland deprecations/updates.

--- nixos/hive.nix
+++ nixos/hive.nix
@@ -1,9 +1,4 @@
 let
-  nixos-25_11 = import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/nixos-25.11.tar.gz";
-    sha256 = "0ln4yw7z3g9lb0x081hc0pd2j1wsx2qqf6bgmwwvdbkcl4bcy1dp";
-  }) { system = "x86_64-linux"; };
-
   nixos-26_05 = import (fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/nixos-26.05.tar.gz";
     sha256 = "1parjl7b3gbmsyj05172pyi3ymz8xqv7bhaa9dca9x1p91cg8fv2";
@@ -14,10 +9,6 @@ in
   meta = {
     nixpkgs = nixos-26_05;
 
-    nodeNixpkgs = {
-      brahms = nixos-26_05;
-      verdi = nixos-25_11;
-    };
   };
 
   defaults =
